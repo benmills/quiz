03454a5eeb604105e4aee6e7c5fa1b8f04ad0d14
lionelbarrow <lionelbarrow@gmail.com>
2013-08-31 20:53:13 -0500

    one day i wont forget to gofmt

--- quiz_test.go
+++ quiz_test.go
@@ -1,137 +1,137 @@
 package quiz
 
 import (
-  "testing"
-  "strings"
+	"strings"
+	"testing"
 )
 
 func newHarness() *testHelperHarness {
-  return &testHelperHarness{false, ""}
+	return &testHelperHarness{false, ""}
 }
 
 type testHelperHarness struct {
-  Failed bool
-  Message string
+	Failed  bool
+	Message string
 }
 
 func (t *testHelperHarness) Fail() {
-  t.Failed = true
+	t.Failed = true
 }
 
 func (t *testHelperHarness) Log(line string) {
-  t.Message = line
+	t.Message = line
 }
 
 func (t *testHelperHarness) Expect(target interface{}) *Expectation {
-  return &Expectation{t: t, target: target}
+	return &Expectation{t: t, target: target}
 }
 
 func TestToEqual(t *testing.T) {
 	h := newHarness()
 
-  h.Expect(2).ToEqual(2)
+	h.Expect(2).ToEqual(2)
 
-  if h.Failed {
-    t.Fail()
-  }
+	if h.Failed {
+		t.Fail()
+	}
 }
 
 func TestToEqualFail(t *testing.T) {
 	h := newHarness()
 
-  h.Expect(1).ToEqual(2)
+	h.Expect(1).ToEqual(2)
 
-  if !strings.Contains(h.Message, "Expected 2 to equal 1.") {
-    t.Fail()
-  }
+	if !strings.Contains(h.Message, "Expected 2 to equal 1.") {
+		t.Fail()
+	}
 }
 
 func TestToBeFalse(t *testing.T) {
-  h := newHarness()
+	h := newHarness()
 
-  h.Expect(1 == 2).ToBeFalse()
+	h.Expect(1 == 2).ToBeFalse()
 
-  if h.Failed {
-    t.Fail()
-  }
+	if h.Failed {
+		t.Fail()
+	}
 }
 
 func TestToBeFalseFail(t *testing.T) {
-  h := newHarness()
+	h := newHarness()
 
-  h.Expect(2 == 2).ToBeFalse()
+	h.Expect(2 == 2).ToBeFalse()
 
-  if !strings.Contains(h.Message, "Expected true to be false") {
-    t.Fail()
-  }
+	if !strings.Contains(h.Message, "Expected true to be false") {
+		t.Fail()
+	}
 }
 
 func TestToBeTrue(t *testing.T) {
-  h := newHarness()
+	h := newHarness()
 
-  h.Expect(1 == 1).ToBeTrue()
+	h.Expect(1 == 1).ToBeTrue()
 
-  if h.Failed {
-    t.Fail()
-  }
+	if h.Failed {
+		t.Fail()
+	}
 }
 
 func TestToBeTrueFail(t *testing.T) {
-  h := newHarness()
+	h := newHarness()
 
-  h.Expect(2 == 3).ToBeTrue()
+	h.Expect(2 == 3).ToBeTrue()
 
-  if !strings.Contains(h.Message, "Expected false to be true") {
-    t.Fail()
-  }
+	if !strings.Contains(h.Message, "Expected false to be true") {
+		t.Fail()
+	}
 }
 
 func TestToBeGreaterThan(t *testing.T) {
-  h := newHarness()
+	h := newHarness()
 
-  h.Expect(1).ToBeGreaterThan(0)
+	h.Expect(1).ToBeGreaterThan(0)
 
-  if h.Failed {
-    t.Fail()
-  }
+	if h.Failed {
+		t.Fail()
+	}
 }
 
 func TestToBeGreaterThanFail(t *testing.T) {
-  h := newHarness()
+	h := newHarness()
 
-  h.Expect(0).ToBeGreaterThan(1)
+	h.Expect(0).ToBeGreaterThan(1)
 
-  if !strings.Contains(h.Message, "Expected 0 to be greater than 1") {
-    t.Fail()
-  }
+	if !strings.Contains(h.Message, "Expected 0 to be greater than 1") {
+		t.Fail()
+	}
 }
 
 func TestToBeLessThan(t *testing.T) {
-  h := newHarness()
+	h := newHarness()
 
-  h.Expect(1).ToBeLessThan(2)
+	h.Expect(1).ToBeLessThan(2)
 
-  if h.Failed {
-    t.Fail()
-  }
+	if h.Failed {
+		t.Fail()
+	}
 }
 
 func TestToContain(t *testing.T) {
-  h := newHarness()
+	h := newHarness()
 
-  h.Expect("Hello world").ToContain("world")
+	h.Expect("Hello world").ToContain("world")
 
-  if h.Failed {
-    t.Fail()
-  }
+	if h.Failed {
+		t.Fail()
+	}
 }
 
 func TestToContainFail(t *testing.T) {
-  h := newHarness()
+	h := newHarness()
 
-  h.Expect("Hello world").ToContain("Goodbye")
+	h.Expect("Hello world").ToContain("Goodbye")
 
-  if !strings.Contains(h.Message, "Expected Hello world to contain Goodbye") {
-    t.Fail()
-  }
+	if !strings.Contains(h.Message, "Expected Hello world to contain Goodbye") {
+		t.Fail()
+	}
 }
